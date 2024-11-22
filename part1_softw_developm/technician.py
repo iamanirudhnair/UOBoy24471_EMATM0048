6b2f68dcfb5e20f0b338e116354ec2d1faba0208
Anirudh Nair <iamanirudhnair@gmail.com>
2024-11-22 17:33:24 +0000

    initial commit

---
+++ part1_softw_developm/technician.py
@@ -0,0 +1,10 @@
+class Technician:
+
+    def __init__(self, name, weekly_rate=500):
+
+        self.name = name
+        self.weekly_rate = weekly_rate
+
+    def get_payment(self):
+
+        return self.weekly_rate * 9  # 9 weeks of work per quarter
