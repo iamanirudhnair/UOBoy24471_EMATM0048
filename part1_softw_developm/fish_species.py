59e4c64c9b0500c10992755bbc5d875020e053a3
Anirudh Nair <iamanirudhnair@gmail.com>
2024-11-24 17:50:58 +0000

    mod commit

--- part1_softw_developm/fish_species.py
+++ part1_softw_developm/fish_species.py
@@ -1,7 +1,20 @@
 class FishSpecies:
+    """
+    The class of Fish Species represents fish types, with specific needs and market details.
+    """
 
     def __init__(self, name, fertilizer, feed, salt, maintenance_time, demand, price):
+        """
+        Initializes fish species with particular details.
 
+        :param name: Fish species name
+        :param fertilizer: Quantity of fertilizer needed for 1 fish 
+        :param feed: Portion of feed required to produce 1 fish
+        :param salt: Amount of salt required for one fish
+        :param maintenance_time: Number of days took to raise 1 fish
+        :param demand: Maximum fish number that can be sold in 1 quarter
+        :param price: Price of 1 fish unit
+        """
         self.name = name
         self.fertilizer = fertilizer  # in ml
         self.feed = feed  # in kg
--- part1_softw_developm/vendor.py
+++ part1_softw_developm/vendor.py
@@ -1,6 +1,17 @@
 class Vendor:
-
+    """
+    The Vendor class stands for the supplier of the hatchery.
+    It stores the information about prices of various supplies sold by the vendor.
+    """
     def __init__(self, name, fertilizer_price, feed_price, salt_price):
+        """
+        Initializes a vendor with its name and prices for supplies.
+
+        :param name: Vendor's name
+        :param fertilizer_price: Per unit of fertilizer's price
+        :param feed_price: Price per unit of feed
+        :param salt_price: Per unit of salt's price
+        """
 
         self.name = name
         self.prices = {
@@ -10,5 +21,10 @@ class Vendor:
         }
 
     def get_price(self, supply_type):
+        """
+        Returns the price for provided supply type from particular vendor.
 
+        :param supply_type: Supply type (fertilizer, feed or salt)
+        :return: Price for the specified supply type
+        """
         return self.prices.get(supply_type, 0)
